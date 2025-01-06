92bf02d7eb8eff2e9551c93e8dd8b4a97d49be2d
Aly Thobani <athobani9@gmail.com>
2025-01-06 12:52:28 -0800

    feat: use declarativeContent to only allow action on reddit pages

--- src/background.ts
+++ src/background.ts
@@ -1,16 +1,37 @@
+const REDDIT_PAGE_URL_PATTERN = /https?:\/\/(\w+\.)?reddit\.com\/r\//;
+const REDDIT_HOST_PATTERN = /(\w+\.)?reddit\.com/;
+
 async function openTabInUnddit(tab: chrome.tabs.Tab): Promise<void> {
   const { url } = tab;
   if (url === undefined) {
-    console.error("The current page URL is undefined.");
+    console.warn("The current page URL is undefined.");
     return;
   }
-  const redditUrlPattern = /(www\.)?reddit\.com/;
-  if (!redditUrlPattern.test(url)) {
-    console.warn("The current page is not a Reddit page.");
+  if (!REDDIT_PAGE_URL_PATTERN.test(url)) {
+    console.warn("Can only open Unddit on subreddit/post/comment pages.", url);
     return;
   }
-  const newUrl = url.replace(redditUrlPattern, "undelete.pullpush.io");
-  await chrome.tabs.create({ url: newUrl });
+  const newUrl = url.replace(REDDIT_HOST_PATTERN, "undelete.pullpush.io");
+  await chrome.tabs.create({
+    url: newUrl,
+    openerTabId: tab.id,
+    index: tab.index + 1,
+  });
 }
 
 chrome.action.onClicked.addListener((tab) => void openTabInUnddit(tab));
+
+chrome.runtime.onInstalled.addListener((_details) => {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    const redditPageStateMatcher =
+      new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: { hostSuffix: ".reddit.com", schemes: ["http", "https"] },
+      });
+    const showActionOnRedditPageRule = {
+      conditions: [redditPageStateMatcher],
+      actions: [new chrome.declarativeContent.ShowAction()],
+    };
+    const rules = [showActionOnRedditPageRule];
+    chrome.declarativeContent.onPageChanged.addRules(rules);
+  });
+});
